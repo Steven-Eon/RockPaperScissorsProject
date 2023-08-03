25a14fd366de5eb3196cf30692338aaf1b3f6c8a
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-03 15:20:28 -0700

    Adding user choice function!

--- script.js
+++ script.js
@@ -7,4 +7,30 @@ function getComputerChoice() {
     return choices[computerChoice];
 }
 
-getComputerChoice();
+function getUserChoice() {
+    console.log("Make your choice! Rock, Paper, or Scissors?");
+    let userPick = prompt("Your pick: ");
+    while (1) {
+        if (typeof(userPick) == "string")
+        {
+            userPick = userPick.toLowerCase();
+            switch (userPick)
+            {
+                case "rock":
+                    console.log(`User chooses ${userPick}!`)
+                    return "rock";
+                    break;
+                case "paper":
+                    console.log(`User chooses ${userPick}!`)
+                    return "paper";
+                    break;
+                case "scissors":
+                    console.log(`User chooses ${userPick}!`)
+                    return "scissors";
+                    break;
+            }
+        }
+        userPick = prompt("Invalid choice, please try again.");
+    }
+}
+
