aff5bafef864b1c871b4829e90ee6653d3eb501a
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-05 03:51:51 -0700

    Game functional, DOM manipulation WIP.

--- scriptRevision.js
+++ scriptRevision.js
@@ -1,4 +1,8 @@
 function initialize() {
+    let choiceList = ["rock", "paper", "scissors"];
+    let userChoice;
+    let computerChoice;
+    let roundWinner;
     const buttonElements = document.querySelectorAll(".userButtonSel button");
     const buttonRock = buttonElements[0];
     const buttonPaper = buttonElements[1];
@@ -6,13 +10,93 @@ function initialize() {
 
     buttonRock.addEventListener('click', (e) => {
         console.log("User has selected ROCK.");
+        userChoice = choiceList[0];
+        computerChoice = processCPUMove();
+        roundWinner = playRound(userChoice, computerChoice);
+        processRound(roundWinner);
     });
     buttonPaper.addEventListener('click', (e) => {
         console.log("User has selected PAPER.")
+        userChoice = choiceList[1];
+        computerChoice = processCPUMove();
+        roundWinner = playRound(userChoice, computerChoice);
+        processRound(roundWinner);
     });
     buttonScissors.addEventListener('click', (e) => {
         console.log("User has selected SCISSORS.")
+        userChoice = choiceList[2];
+        computerChoice = processCPUMove();
+        roundWinner = playRound(userChoice, computerChoice);
+        processRound(roundWinner);
     });
 }
 
-initialize();
+function processCPUMove() {
+    let choiceList = ["rock", "paper", "scissors"];
+    const computerChoice = Math.floor(Math.random() * 3);
+    console.log(`CPU chooses ${choiceList[computerChoice]}!`);
+    return choiceList[computerChoice];
+}
+
+function playRound(userChoice, cpuChoice)
+{
+    if (userChoice == cpuChoice)
+    {
+        console.log("It's a draw!");
+        return 0;
+    }
+    else
+    {
+        switch (userChoice)
+        {
+            case "rock":
+                if (cpuChoice == "paper")
+                {
+                    console.log("CPU Wins!");
+                    return 2;
+                }
+                else
+                {
+                    console.log("User Wins!");
+                    return 1;
+                }
+                break;
+            case "paper":
+                if (cpuChoice == "scissors")
+                {
+                    console.log("CPU Wins!");
+                    return 2;
+                }
+                else
+                {
+                    console.log("User Wins!");
+                    return 1;
+                }
+                break;
+            case "scissors":
+                if (cpuChoice == "rock")
+                {
+                    console.log("CPU Wins!");
+                    return 2;
+                }
+                else
+                {
+                    console.log("User Wins!");
+                    return 1;
+                }
+                break;
+        }
+    }
+}
+
+function processRound(results) {
+    if (results == 0)
+    {
+        return;
+    }
+    const score = document.querySelectorAll(".scoreSection > div > p");
+    const scoreToModify = score[(results * 2) - 1]
+    scoreToModify.textContent = scoreToModify.textContent + 1;
+}
+
+initialize()
